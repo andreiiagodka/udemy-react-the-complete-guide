4f4907640ddb669332bd1f4a19404b4743a7c88f
Andrey Yagodka <andrei.iagodka@gmail.com>
2020-11-27 05:55:53 +0200

    Add actions for ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -29,14 +29,13 @@ class BurgerBuilder extends Component {
 
   componentDidMount() {
     console.log(this.props)
-    axios.get('https://burger-builder-34ea0.firebaseio.com/ingredients.json')
-      .then(response => {
-        this.setState({ingredients: response.data})
-      })
-      .catch(error => {
-        this.setState({error: true})
-      })
-
+    // axios.get('https://burger-builder-34ea0.firebaseio.com/ingredients.json')
+    //   .then(response => {
+    //     this.setState({ingredients: response.data})
+    //   })
+    //   .catch(error => {
+    //     this.setState({error: true})
+    //   })
   }
 
   updatePurchaseState = (ingredients) => {
