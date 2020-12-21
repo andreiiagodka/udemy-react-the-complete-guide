dd5b59e83e600edbda1633f19665d48a17bcd29e
Andrey Yagodka <andrei.iagodka@gmail.com>
2020-12-21 08:18:10 +0200

    Add sign in

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -22,7 +22,7 @@ export const authFail = (error) => {
   }
 }
 
-export const auth = (email, password) => {
+export const auth = (email, password, isSignup) => {
   return dispatch => {
     dispatch(authStart())
 
@@ -31,7 +31,12 @@ export const auth = (email, password) => {
       password: password,
       returnSecureToken: true
     }
-    axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyD6ciJeaI4m6prsLB1GWD5AEaboZwAMpYs', authData)
+    let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyD6ciJeaI4m6prsLB1GWD5AEaboZwAMpYs'
+    if (!isSignup) {
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyD6ciJeaI4m6prsLB1GWD5AEaboZwAMpYs'
+    }
+
+    axios.post(url, authData)
       .then(response => {
         console.log(response);
         dispatch(authSuccess(response.data))
