08c897ac148eeab56354c25bef625b94dd662d1c
Andrey Yagodka <andrei.iagodka@gmail.com>
2020-12-28 08:07:16 +0200

    Persist auth state with localStorage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import * as actions from './store/actions/index';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
@@ -8,30 +11,42 @@ import Orders from './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
 
-function App() {
-  return (
-    <div>
-      <Layout>
-        <Switch>
-          <Route path='/checkout'>
-            <Checkout />
-          </Route>
-          <Route path='/orders'>
-            <Orders />
-          </Route>
-          <Route path='/auth'>
-            <Auth />
-          </Route>
-          <Route path='/logout'>
-            <Logout />
-          </Route>
-          <Route to='/' exact>
-            <BurgerBuilder />
-          </Route>
-        </Switch>
-      </Layout>
-    </div>
-  );
+class App extends Component {
+  componentDidMount () {
+    this.props.onTryAutoSignup()
+  }
+
+  render () {
+    return (
+      <div>
+        <Layout>
+          <Switch>
+            <Route path='/checkout'>
+              <Checkout />
+            </Route>
+            <Route path='/orders'>
+              <Orders />
+            </Route>
+            <Route path='/auth'>
+              <Auth />
+            </Route>
+            <Route path='/logout'>
+              <Logout />
+            </Route>
+            <Route to='/' exact>
+              <BurgerBuilder />
+            </Route>
+          </Switch>
+        </Layout>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => {
+  return {
+    onTryAutoSignup: () => dispatch(actions.authCheckState())
+  }
+}
+
+export default connect(null, mapDispatchToProps)(App);
