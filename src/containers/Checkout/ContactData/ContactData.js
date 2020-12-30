585b9166549a6086212349f5fc59265eb7ecdeca
Andrey Yagodka <andrei.iagodka@gmail.com>
2020-12-30 14:24:25 +0200

    Share validation method

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -9,7 +9,7 @@ import Spinner from '../../../components/UI/Spinner/Spinner';
 import Input from '../../../components/UI/Input/Input';
 import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler';
 import * as actions from '../../../store/actions/index';
-import { updateObject } from '../../../shared/utility';
+import { updateObject, checkValidity } from '../../../shared/utility';
 
 class ContactData extends Component {
   state = {
@@ -97,24 +97,6 @@ class ContactData extends Component {
     formIsValid: false
   }
 
-  checkValidity(value, rules) {
-    let isValid = true
-
-    if (rules.required) {
-      isValid = value.trim() !== '' && isValid
-    }
-
-    if (rules.minLength) {
-      isValid = value.length >= rules.minLength && isValid
-    }
-
-    if (rules.maxLength) {
-      isValid = value.length <= rules.maxLength && isValid
-    }
-
-    return isValid
-  }
-
   orderHandler = (event) => {
     event.preventDefault()
 
@@ -135,7 +117,7 @@ class ContactData extends Component {
   inputChangedHandler = (event, inputId) => {
     const updatedFormElement = updateObject(this.state.orderForm[inputId], {
       value: event.target.value,
-      valid: this.checkValidity(event.target.value, this.state.orderForm[inputId].validation),
+      valid: checkValidity(event.target.value, this.state.orderForm[inputId].validation),
       touched: true
     })
     const updatedOrderForm = updateObject(this.state.orderForm, {
