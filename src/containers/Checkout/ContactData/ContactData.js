5aeeae578aee20f600f5cc4fe35dd50544425c3d
Andrey Yagodka <andrei.iagodka@gmail.com>
2020-12-30 14:20:50 +0200

    Use updateObject in the entire app

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -9,6 +9,7 @@ import Spinner from '../../../components/UI/Spinner/Spinner';
 import Input from '../../../components/UI/Input/Input';
 import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler';
 import * as actions from '../../../store/actions/index';
+import { updateObject } from '../../../shared/utility';
 
 class ContactData extends Component {
   state = {
@@ -132,13 +133,14 @@ class ContactData extends Component {
   }
 
   inputChangedHandler = (event, inputId) => {
-    const updatedOrderForm = { ...this.state.orderForm }
-    const updatedFormElement = { ...updatedOrderForm[inputId] }
-
-    updatedFormElement.value = event.target.value
-    updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation) 
-    updatedFormElement.touched = true
-    updatedOrderForm[inputId] = updatedFormElement
+    const updatedFormElement = updateObject(this.state.orderForm[inputId], {
+      value: event.target.value,
+      valid: this.checkValidity(event.target.value, this.state.orderForm[inputId].validation),
+      touched: true
+    })
+    const updatedOrderForm = updateObject(this.state.orderForm, {
+      [inputId]: updatedFormElement
+    })
 
     let formIsValid = true
     for (let name in updatedOrderForm) {
